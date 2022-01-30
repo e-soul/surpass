c1ffe98cda2ce262ba3ba3e0f9f77893658456b7
e-soul <esoulorg@gmail.com>
2022-01-30 15:12:29 +0200

    Update copyright statements for 2022.

--- surpass.gui/src/main/java/org/esoul/surpass/gui/ExitProgrammeHandler.java
+++ surpass.gui/src/main/java/org/esoul/surpass/gui/ExitProgrammeHandler.java
@@ -1,5 +1,5 @@
 /*
-   Copyright 2017-2020 e-soul.org
+   Copyright 2017-2022 e-soul.org
    All rights reserved.
 
    Redistribution and use in source and binary forms, with or without modification, are permitted
