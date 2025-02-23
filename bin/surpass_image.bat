6697d350dd83e3475c03d54b7cbfd6e7ef9cb584
e-soul contrib1 <esoulorgcontrib1@gmail.com>
2025-02-23 18:14:40 +0100

    Do not show the console window.

--- bin/surpass_image.bat
+++ bin/surpass_image.bat
@@ -3,4 +3,4 @@
 set SCRIPT_DIR=%~dp0
 if %SCRIPT_DIR:~-1%==\ set SCRIPT_DIR=%SCRIPT_DIR:~0,-1%
 REM set VM_OPTIONS="-Dorg.esoul.surpass.persist.datadir=%SCRIPT_DIR%"
-"%SCRIPT_DIR%\bin\java" %VM_OPTIONS% -m surpass.gui/org.esoul.surpass.gui.Main %*
+"%SCRIPT_DIR%\bin\javaw" %VM_OPTIONS% -m surpass.gui/org.esoul.surpass.gui.Main %*
