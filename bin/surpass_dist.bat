d4b292fbe5d70c92827b8810448ba09f8f6c23a5
e-soul contrib1 <esoulorgcontrib1@gmail.com>
2025-06-01 18:18:52 +0200

    Enable native access for flatlaf.

--- bin/surpass_dist.bat
+++ bin/surpass_dist.bat
@@ -4,4 +4,4 @@ set SCRIPT_DIR=%~dp0
 if %SCRIPT_DIR:~-1%==\ set SCRIPT_DIR=%SCRIPT_DIR:~0,-1%
 REM set VM_OPTIONS=-Dorg.esoul.surpass.persist.datadir="%SCRIPT_DIR%"
 REM set VM_OPTIONS=%VM_OPTIONS% -Dorg.esoul.surpass.laf=com.formdev.flatlaf.FlatLightLaf
-"%SCRIPT_DIR%\jre\bin\javaw" %VM_OPTIONS% -p "%SCRIPT_DIR%\mods" --add-modules ALL-MODULE-PATH,ALL-SYSTEM -m surpass.gui/org.esoul.surpass.gui.Main %*
+"%SCRIPT_DIR%\jre\bin\javaw" %VM_OPTIONS% --enable-native-access=com.formdev.flatlaf -p "%SCRIPT_DIR%\mods" --add-modules ALL-MODULE-PATH,ALL-SYSTEM -m surpass.gui/org.esoul.surpass.gui.Main %*
